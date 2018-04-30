7c8ed69bfdd6e90114f651bb61a3e19453bc59b2
Tom Martens <tommiemartens@hotmail.com>
2018-04-30 20:33:13 +0200

    [Added] Player class

--- src/Game/GameDrawer.java
+++ src/Game/GameDrawer.java
@@ -1,5 +1,6 @@
 package game;
 
+import game.charachter.Player;
 import game.map.TiledMap;
 
 import javax.swing.*;
@@ -8,14 +9,17 @@ import java.awt.geom.AffineTransform;
 
 public class GameDrawer extends JPanel
 {
+    Player player;
 
     @Override
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
+        player =  new Player(new Point(0,0),"gay");
         AffineTransform tx = new AffineTransform();
         TiledMap map = new TiledMap("res/map/map.json");
         map.debugDraw(g2d, tx);
+        player.drawPlayer(g2d);
     }
 }
